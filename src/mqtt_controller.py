7d8b3d5be5844e7fe9d20285b9f5d9cc2cf711a3
Tobias Weiland <post@tobias-weiland.eu>
2025-05-02 11:47:44 +0200

    update mqtt topic to urbain-air-lab schema

--- src/mqtt_controller.py
+++ src/mqtt_controller.py
@@ -48,7 +48,7 @@ class MQTTController:
     def publish_data(self, data: Dict[str, Any]) -> None:
         data["tele"]["packet_count"] = self._get_next_packet_count()
         json_data = json.dumps(data, indent=4)
-        self.client.publish(config.MQTT_BASE_TOPIC + "/" + config.NODE_ID, json_data, qos=2)
+        self.client.publish("sensors" + "/" + config.MQTT_BASE_TOPIC + "/" + config.NODE_ID, json_data, qos=2)
         #print("mqtt publish: ", data)
 
     def stop(self) -> None:
